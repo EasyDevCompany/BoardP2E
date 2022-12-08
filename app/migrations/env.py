6078d4e0694f8be844633c88f9fe3c529fcb8f49
Axel791 <HS.Kazakov@mail.ru>
2022-12-08 23:53:30 +0300

    create model

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -57,6 +57,7 @@ def run_migrations_offline():
     with context.begin_transaction():
         context.run_migrations()
 
+
 def do_run_migrations(connection):
     context.configure(
         connection=connection, target_metadata=target_metadata, compare_type=True
