d350baa7c3fe1cc69a8f3a020146ec25e63b34ec
EasyDev <118206947+EasyDevCompany@users.noreply.github.com>
2022-12-16 22:09:08 +0000

    unlock handler and add new endpoint

--- app/app/api/api_v1/endpoints/user_auth.py
+++ app/app/api/api_v1/endpoints/user_auth.py
@@ -29,12 +29,18 @@ async def registration(
     return await auth_service.registration(user=user)
 
 
-# @router.post('/profile', response_model=MyProfileOut)
-# @inject
-# async def my_profile(
-#     #! Сюда нужно вставить функцию получения айди по тому какой пользователь обращается
-#         # user_id: str = Depends(get_current_user_auth),
-#         registration_service=Depends(Provide[Container.registration_service])):
-#     """Личный кабинет."""
-#     return registration_service.my_profile(user_id="ID") 
+@router.post('/profile', response_model=MyProfileOut)
+@inject
+async def my_profile(
+    #! Сюда нужно вставить функцию получения айди по тому какой пользователь обращается
+        # user_id: str = Depends(get_current_user_auth),
+        registration_service=Depends(Provide[Container.registration_service])):
+    """Личный кабинет."""
+    return registration_service.my_profile(user_id="ID")
+
+
+@router.post
+@inject
+async def get_post():
+    return {"Hello world": "Hello world!"}
 
